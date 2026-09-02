3a894f758e10eb7f19eddf434e42498e6255d4c9
guilhermebenvenutti <ens-guilhermebenvenutti@ugv.edu.br>
2026-09-01 21:35:56 -0300

    style: carrega estilos dos componentes reutilizáveis

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/variables.css'
 import './styles/global.css'
+import './components/ui/ui.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
